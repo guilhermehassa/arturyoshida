dde654df63de7e8c95c37449b24c9bdd4d2f6920
Guilherme Hassã <91090230+guilhermehassa@users.noreply.github.com>
2025-07-05 17:49:35 -0300

    About Section and fix Header fixed

--- wp-content/themes/understrap-child-main/page-templates/lp.php
+++ wp-content/themes/understrap-child-main/page-templates/lp.php
@@ -10,7 +10,7 @@ defined( 'ABSPATH' ) || exit;
 
 get_header();
 ?>
-<div class="wrapper" id="page-wrapper">
+<main class="wrapper" id="page-wrapper">
   <section class="hero">
     <div class="container">
       <div class="row justify-content-center">
@@ -21,8 +21,68 @@ get_header();
       </div>
     </div>
   </section>
+  <section class="about" id="sobremim">
+    <div class="container">
+      <div class="row justify-content-center">
+        <div class="col-10 col-lg-3 about_title">
+          <h2>Quem é<br>Artur Yoshida?</h2>
+          <img src="<?= get_stylesheet_directory_uri();?>/img/arturyoshida.png" alt="">
+        </div>
+        <div class="col-12 col-lg-8 offset-lg-1 about_text">
+          <h3>
+            CARREIRA
+          </h3>
+          <p>
+            Artur de Padua Yoshida de Oliveira ou como é conhecido “Dr. Artur Yoshida”. Nascido em São Bernardo do Campo, é advogado desde 14 de março de 2014, Em mais de 11 anos de experiência atuando nas áreas de Direito Civil, especialmente em inventários, usucapiões e empresas, com consultoria preventiva e estratégica em processos judiciais e extrajudiciais complexos, é Pós-graduado em Advocacia Cível pela Escola Brasileira de Direito (EBRADI) e pós-graduando em Direito Societário pela Pontifícia Universidade Católica do Rio Grande do Sul (PUCRS) reforçando a graduação em Direito celebrada na cidade de Santos/SP, pela Escola Superior de Administração, Marketing e Comunicação (ESAMC – Santos).
+          </p>
+          <h3>
+            CARREIRA
+          </h3>
+          <p>
+            Artur de Padua Yoshida de Oliveira ou como é conhecido “Dr. Artur Yoshida”. Nascido em São Bernardo do Campo, é advogado desde 14 de março de 2014, Em mais de 11 anos de experiência atuando nas áreas de Direito Civil, especialmente em inventários, usucapiões e empresas, com consultoria preventiva e estratégica em processos judiciais e extrajudiciais complexos, é Pós-graduado em Advocacia Cível pela Escola Brasileira de Direito (EBRADI) e pós-graduando em Direito Societário pela Pontifícia Universidade Católica do Rio Grande do Sul (PUCRS) reforçando a graduação em Direito celebrada na cidade de Santos/SP, pela Escola Superior de Administração, Marketing e Comunicação (ESAMC – Santos).
+          </p>
+          <h3>
+            CARREIRA
+          </h3>
+          <p>
+            Artur de Padua Yoshida de Oliveira ou como é conhecido “Dr. Artur Yoshida”. Nascido em São Bernardo do Campo, é advogado desde 14 de março de 2014, Em mais de 11 anos de experiência atuando nas áreas de Direito Civil, especialmente em inventários, usucapiões e empresas, com consultoria preventiva e estratégica em processos judiciais e extrajudiciais complexos, é Pós-graduado em Advocacia Cível pela Escola Brasileira de Direito (EBRADI) e pós-graduando em Direito Societário pela Pontifícia Universidade Católica do Rio Grande do Sul (PUCRS) reforçando a graduação em Direito celebrada na cidade de Santos/SP, pela Escola Superior de Administração, Marketing e Comunicação (ESAMC – Santos).
+          </p>
+          <h3>
+            CARREIRA
+          </h3>
+          <p>
+            Artur de Padua Yoshida de Oliveira ou como é conhecido “Dr. Artur Yoshida”. Nascido em São Bernardo do Campo, é advogado desde 14 de março de 2014, Em mais de 11 anos de experiência atuando nas áreas de Direito Civil, especialmente em inventários, usucapiões e empresas, com consultoria preventiva e estratégica em processos judiciais e extrajudiciais complexos, é Pós-graduado em Advocacia Cível pela Escola Brasileira de Direito (EBRADI) e pós-graduando em Direito Societário pela Pontifícia Universidade Católica do Rio Grande do Sul (PUCRS) reforçando a graduação em Direito celebrada na cidade de Santos/SP, pela Escola Superior de Administração, Marketing e Comunicação (ESAMC – Santos).
+          </p>
+          <h3>
+            CARREIRA
+          </h3>
+          <p>
+            Artur de Padua Yoshida de Oliveira ou como é conhecido “Dr. Artur Yoshida”. Nascido em São Bernardo do Campo, é advogado desde 14 de março de 2014, Em mais de 11 anos de experiência atuando nas áreas de Direito Civil, especialmente em inventários, usucapiões e empresas, com consultoria preventiva e estratégica em processos judiciais e extrajudiciais complexos, é Pós-graduado em Advocacia Cível pela Escola Brasileira de Direito (EBRADI) e pós-graduando em Direito Societário pela Pontifícia Universidade Católica do Rio Grande do Sul (PUCRS) reforçando a graduação em Direito celebrada na cidade de Santos/SP, pela Escola Superior de Administração, Marketing e Comunicação (ESAMC – Santos).
+          </p>
+          <h3>
+            CARREIRA
+          </h3>
+          <p>
+            Artur de Padua Yoshida de Oliveira ou como é conhecido “Dr. Artur Yoshida”. Nascido em São Bernardo do Campo, é advogado desde 14 de março de 2014, Em mais de 11 anos de experiência atuando nas áreas de Direito Civil, especialmente em inventários, usucapiões e empresas, com consultoria preventiva e estratégica em processos judiciais e extrajudiciais complexos, é Pós-graduado em Advocacia Cível pela Escola Brasileira de Direito (EBRADI) e pós-graduando em Direito Societário pela Pontifícia Universidade Católica do Rio Grande do Sul (PUCRS) reforçando a graduação em Direito celebrada na cidade de Santos/SP, pela Escola Superior de Administração, Marketing e Comunicação (ESAMC – Santos).
+          </p>
+          <h3>
+            CARREIRA
+          </h3>
+          <p>
+            Artur de Padua Yoshida de Oliveira ou como é conhecido “Dr. Artur Yoshida”. Nascido em São Bernardo do Campo, é advogado desde 14 de março de 2014, Em mais de 11 anos de experiência atuando nas áreas de Direito Civil, especialmente em inventários, usucapiões e empresas, com consultoria preventiva e estratégica em processos judiciais e extrajudiciais complexos, é Pós-graduado em Advocacia Cível pela Escola Brasileira de Direito (EBRADI) e pós-graduando em Direito Societário pela Pontifícia Universidade Católica do Rio Grande do Sul (PUCRS) reforçando a graduação em Direito celebrada na cidade de Santos/SP, pela Escola Superior de Administração, Marketing e Comunicação (ESAMC – Santos).
+          </p>
+          <h3>
+            CARREIRA
+          </h3>
+          <p>
+            Artur de Padua Yoshida de Oliveira ou como é conhecido “Dr. Artur Yoshida”. Nascido em São Bernardo do Campo, é advogado desde 14 de março de 2014, Em mais de 11 anos de experiência atuando nas áreas de Direito Civil, especialmente em inventários, usucapiões e empresas, com consultoria preventiva e estratégica em processos judiciais e extrajudiciais complexos, é Pós-graduado em Advocacia Cível pela Escola Brasileira de Direito (EBRADI) e pós-graduando em Direito Societário pela Pontifícia Universidade Católica do Rio Grande do Sul (PUCRS) reforçando a graduação em Direito celebrada na cidade de Santos/SP, pela Escola Superior de Administração, Marketing e Comunicação (ESAMC – Santos).
+          </p>
+        </div>
+      </div>
+    </div>
+  </section>
   
-</div>
+</main>
 
 <?php
 get_footer();

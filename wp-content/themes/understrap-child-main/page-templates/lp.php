e0886356c468dbe844a91636d5ad4016766b66ff
Guilherme Hassã <91090230+guilhermehassa@users.noreply.github.com>
2025-07-05 17:09:15 -0300

    Hero and Titles Patterns

--- wp-content/themes/understrap-child-main/page-templates/lp.php
+++ wp-content/themes/understrap-child-main/page-templates/lp.php
@@ -11,6 +11,16 @@ defined( 'ABSPATH' ) || exit;
 get_header();
 ?>
 <div class="wrapper" id="page-wrapper">
+  <section class="hero">
+    <div class="container">
+      <div class="row justify-content-center">
+        <div class="col-11 col-lg-10">
+          <h1 class="h2">Assessoria Jurídica Especializada em Inventários e Empresas.</h1>
+          <a href="#vamosconversar" class="defaultButton" >Vamos Conversar?</a>
+        </div>
+      </div>
+    </div>
+  </section>
   
 </div>
 
